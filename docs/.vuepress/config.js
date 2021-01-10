1a456fd6ee688b4790e36d8d73ebb038110f2487
godbasin <wangbeishan@163.com>
2021-01-10 21:43:52 +0800

    update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -301,6 +301,7 @@ module.exports = {
                         '/front-end-work/front-end-days/about-front-end-8.md',
                         '/front-end-work/front-end-days/about-front-end-9.md',
                         '/front-end-work/front-end-days/about-front-end-10.md',
+                        '/front-end-work/front-end-days/about-writing.md',
                     ]
                 }, {
                     title: '面试这件事',
