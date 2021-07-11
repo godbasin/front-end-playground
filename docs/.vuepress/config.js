27e716e221cace707e4ca474caa1cdae9cd815ef
godbasin <wangbeishan@163.com>
2021-07-11 15:23:37 +0800

    feat: 更新一篇 angular

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -132,6 +132,7 @@ module.exports = {
                         '/front-end-others/deep-into-angular/angular-design-zone-ngzone.md',
                         '/front-end-others/deep-into-angular/angular-design-module.md',
                         '/front-end-others/deep-into-angular/angular-design-di-1-basic-concepts.md',
+                        '/front-end-others/deep-into-angular/angular-design-di-1-basic-concepts.md',
                     ]
                 }, {
                     title: '玩转 Angular',
